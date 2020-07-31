1107087fd3c87056c2773223deb8be9c245014b1
Jeury Suarez Hernandez <49211071+jeuryblackrain@users.noreply.github.com>
2020-07-31 11:58:23 -0400

    TERMINADO

---
+++ Ejercicio de Codificacion VI-III/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_de_Codificacion_VI_III
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===========> LLENANDO UN VECTOR CON NOMBRES <======== \n");
+
+            string[] vector = new string[5];
+
+            //Llena el Vector con los 5 valores
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Espacio del Vector {0}", i);
+                vector[i] = Console.ReadLine();
+            }
+
+            Console.Clear();
+            //Lee Los valores en el vector
+            Console.WriteLine("===========> VECTOR BUCLE I <======== \n");
+            for (int x = 0; x < 5; x++)
+            {
+                Console.WriteLine("Vector {0} -[{1}]", x, vector[x]);
+            }
+
+            Console.WriteLine("\n===========> VECTOR BUCLE II <======== \n");
+            int y = 0; 
+            while(y < 5)
+            {
+                Console.WriteLine("Vector {0} -[{1}]", y, vector[y]);
+                y++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio de Codificacion VI-IV/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_de_Codificacion_VI_IV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===========> LLENANDO UN VECTOR <======== \n");
+
+            int[,] vectorA = new int[2,2];
+            int[,] vectorB = new int[2,2];
+            int[,] vectorC = new int[2,2];
+
+
+            Console.WriteLine("\n==========> Vector A <========== \n");
+            for (int i = 0; i < 2; i++)
+            {
+                for (int y = 0; y < 2; y++)
+                {
+                    Console.Write("Posicion del vector A({0} {1}): ",i, y, vectorA[i,y]);
+                    vectorA[i,y] = Convert.ToInt32(Console.ReadLine());                 
+                }
+            }
+            Console.WriteLine("\n==========> Vector B <========== \n");
+            for (int z = 0; z < 2; z++)
+            {
+                for (int c = 0; c < 2; c++)
+                {
+                    Console.Write("Posicion del vector A({0} {1}): ", z, c, vectorA[z, c]);
+                    vectorB[z, c] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            //Suma los Vectores A y B
+            for (int o = 0; o < 2; o++)
+            {
+                for (int c = 0; c < 2; c++)
+                {
+                    vectorC[o,c] = vectorA[o,c] + vectorB[o,c];
+                }
+            }
+
+
+            Console.Clear();
+            //Lee Los valores en el vector
+            Console.Write("===========> VECTOR A <========");
+            for (int x = 0; x < 2; x++)
+            {
+                Console.WriteLine("");
+                for (int p = 0; p < 2; p++)
+                {
+                    Console.Write("|" + vectorA[x,p] + "| ");            
+                }
+            }
+
+            Console.Write("\n===========> VECTOR B <========");
+            for (int q = 0; q < 2; q++)
+            {
+                Console.WriteLine("");
+                for (int p = 0; p < 2; p++)
+                {
+                    Console.Write("  {0}  ", vectorB[q, p]);
+                }
+            }
+
+            Console.Write("\n===========> VECTOR C <========");
+            for (int r = 0; r < 2; r++)
+            {
+                Console.WriteLine("");
+                for (int p = 0; p < 2; p++)
+                {
+                    Console.Write("  {0}  ", vectorC[r, p]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
