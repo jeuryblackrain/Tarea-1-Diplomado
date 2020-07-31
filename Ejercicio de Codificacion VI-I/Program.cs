e5c7bda2edf352e9286217191e8baba9b98e34d4
Jeury Suarez Hernandez <49211071+jeuryblackrain@users.noreply.github.com>
2020-07-30 23:44:18 -0400

    Actualizacion

---
+++ Ejercicio de Codificacion VI-I/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_de_Codificacion_VI_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===========> LLENANDO UN VECTOR <======== \n");
+
+            int[] vector = new int[5];
+
+            //Llena el Vector con los 5 valores
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Espacio del Vector {0}", i);
+                vector[i] = Convert.ToInt32(Console.ReadLine()); 
+            }
+
+            Console.Clear();
+            //Lee Los valores en el vector
+            Console.WriteLine("===========> VECTOR <======== \n");
+            for (int x = 0; x < 5; x++)
+            {
+                Console.WriteLine("Vector {0} -[{1}]",x, vector[x]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio de Codificacion VI-II/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_de_Codificacion_VI_II
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===========> SUMA UN VECTOR <======== \n");
+
+            int[] vector = new int[5];
+            
+
+            //Llena el Vector con los 5 valores
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Espacio del Vector {0}", i);
+                vector[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Clear();
+            //Lee Los valores en el vector
+            int suma = 0;
+            Console.WriteLine("===========> VECTOR <======== \n");
+            for (int x = 0; x < 5; x++)
+            {
+                Console.WriteLine("Vector {0} -[{1}]", x, vector[x]);
+                suma += vector[x];
+           }
+            Console.WriteLine("\nLa Suma de Los Vectores Es: {0}",suma);
+
+
+            Console.ReadKey();
+        }
+    }
+}
